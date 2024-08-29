6c1e6b5297b3ec6d6e0b17f5e730a4fd59a5cd38
SergeyL-club <damnazzel@gmail.com>
2024-08-29 22:32:51 +0500

    fix trade

--- src/main.ts
+++ src/main.ts
@@ -62,6 +62,9 @@ type Broker = {
 };
 
 async function updateCurse(redis: Remote<WorkerRedis>, browser: Remote<WorkerBrowser>) {
+  const evaluteTradeToggleFunc = `getCookie('LAST_SELL_TRADING_ACTIVE')`;
+  const isTrade = (await browser.evalute({ code: evaluteTradeToggleFunc })) as 'true' | 'false';
+  if (isTrade === 'false') return logger.log(`Начало итерации остановлено, т.к. трейд отключен`);
   const limitLots = (await redis.getConfig('POLLING_CURSE_LIMIT')) as number;
   const evaluteFuncLots = `getLots("[authKey]", ${JSON.stringify({ offset: 0, limit: limitLots, page: 1, currency: 'rub' })})`;
   const lots = (await browser.evalute({ code: evaluteFuncLots })) as Lot[] | null;
