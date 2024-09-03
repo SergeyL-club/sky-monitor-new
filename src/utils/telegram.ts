90bf3e7c7d9b31d7ad10474ea97a82a55eb4bbe5
SergeyL-club <damnazzel@gmail.com>
2024-09-03 17:14:33 +0500

    remove debug

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -60,8 +60,7 @@ class TelegramAPI {
     const client = new TelegramClient(new StringSession(String(stringSession)), Number(apiId), apiHash, {
       connectionRetries: 5,
     });
-    console.log(apiId, apiHash, stringSession, botName);
-    client.setLogLevel(LogLevel.DEBUG);
+    client.setLogLevel(LogLevel.NONE);
 
     try {
       await client.start({
